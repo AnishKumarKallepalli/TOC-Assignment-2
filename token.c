acc07914f2a1f9f6c443cd986c7a18d0c1b6aaf4
AnishKumarKallepalli <88476617+AnishKumarKallepalli@users.noreply.github.com>
2022-12-09 21:05:11 +0530

    Updated Tokenizer
    New logic

--- token.c
+++ token.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <math.h>
 
@@ -8,61 +9,87 @@
 #define EXIT_SUCCESS 0
 
 char *tokens[MAX_TOKENS];
-int num_tokens = 0;
-int line_position = 0;
-char str[10000];
+int num_tokens;
+char *delimiters = " ";
 
-void tokenize(char *filename)
+void add_spaces_around_chars(char *str, const char *chars)
 {
+  // Create a copy of the input string
+  char temp[strlen(str) + 1];
+  strcpy(temp, str);
 
-    FILE *in_file = fopen(filename, "r");
+  // Initialize variables
+  char *out = str;
 
-    char line[MAX_TOKENS];
-    while (fgets(line, MAX_LINE_LENGTH, in_file))
+  // Loop through all characters in the string
+  for (int i = 0; i < strlen(temp); i++)
+  {
+    // Check if the current character is in the set of characters
+    if (strchr(chars, temp[i]))
     {
-        for (int i = 0; i < strlen(line); ++i)
-        {
-            if (line[i] == '\t' || line[i] == '\n' || line[i] == '\r')
-                continue;
-            if (line[i] == ' ' || line[i] == ';' || line[i] == ',' || line[i] == '=' || line[i] == '>' || line[i] == '+' || line[i] == '-' || line[i] == '/' || line[i] == '*' || line[i] == '(' || line[i] == ')' || line[i] == '{' || line[i] == '}')
-            {
-                if (strlen(str))
-                {
-                    line_position = 0;
-                    tokens[num_tokens] = (char*)malloc(sizeof(str));
+      // Add a space before the character
+      *out++ = ' ';
 
-                    strcpy(tokens[num_tokens++], str);
-                    memset(str, '\0', sizeof(str));
-                }
+      // Copy the character to the output string
+      *out++ = temp[i];
 
-                if (line[i] == ' ')
-                    continue;
-
-                tokens[num_tokens] = (char*)malloc(2);
-                tokens[num_tokens++][0] = line[i];
-            }
-            else
-            {
-                str[line_position++] = line[i];
-            }
-        }
+      // Add a space after the character
+      *out++ = ' ';
     }
+    else
+    {
+      // Copy the character to the output string
+      *out++ = temp[i];
+    }
+  }
+
+  // Terminate the output string
+  *out = '\0';
+}
+
+int tokenize(char *input, char *tokens[])
+{
+  int num_tokens = 0;
+
+  char *token = strtok(input, delimiters);
+
+  while (token != NULL && num_tokens < MAX_TOKENS)
+  {
+    tokens[num_tokens++] = token;
+    token = strtok(NULL, delimiters);
+  }
+
+  return num_tokens;
 }
 
 int main(int argc, char **argv)
 {
 
-    if (argc < 2)
+  if (argc < 2)
+  {
+    perror("Pass the name of the input file as the first parameter. e.g.: ./simulator input.txt");
+    exit(EXIT_FAILURE);
+  }
+  FILE *in_file = fopen(argv[1], "r");
+  char line[MAX_TOKENS];
+  char str[MAX_TOKENS];
+  int j=0;
+  while (fgets(line, MAX_LINE_LENGTH, in_file))
+  {
+    for (int i = 0; i < strlen(line); ++i)
     {
-        perror("Pass the name of the input file as the first parameter. e.g.: ./simulator input.txt");
-        exit(EXIT_FAILURE);
+      if (line[i] == '\t' || line[i] == '\n' || line[i] == '\r')
+                continue;
+      str[j]=line[i];
+      j++;
     }
-    tokenize(argv[1]);
-    for (int i = 0; i < num_tokens; ++i)
-    {
-        printf("%s\n", tokens[i]);
-    }
-    // printf("\n");
-    // printf("%d\n", num_tokens);
-    return EXIT_SUCCESS;
+  }
+  add_spaces_around_chars(str, ",;=>+-*/(){}");
+  num_tokens = tokenize(str, tokens);
+    for (int i = 0; i < num_tokens; i++)
+      {
+        printf("Token %d: %s\n", i + 1, tokens[i]);
+      }
+
+  return 0;
 }
