e5e91917488c7629c93acf98a276d1fa2035cc8b
Yash Ratnani <86655022+yashratnani02@users.noreply.github.com>
2022-12-12 18:51:11 +0530

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,8 +1,10 @@
 from nltk.tree import *
 
 # assign your output (generalied list of the syntax tree) to varaible text
-text = "[P[D[int][V[Var[b]][,][V[Var[c]]]][;]][S[Sing[Ass[Var[b]][=][Exp[AS[MD[Fact[Const[3]]]][+][AS[MD[Fact[(][Exp[AS[MD[Fact[Const[4]]]]][>][Exp[AS[MD[Fact[Const[5]]]]]]][)]][*][MD[Fact[Const[6]]]]]]][==][Exp[AS[MD[Fact[Const[1]]]]]]]]]]]"
+f = open('syntaxtree.txt', 'r')
 
+text = f.readlines()[0]
+f.close()
 
 text = text.replace("(", "ob")    #in the syntax tree, 'ob' will display in place of '('
 text = text.replace(")", "cb")    #in the syntax tree, 'cb' will display in place of ')'
@@ -11,5 +13,4 @@ text = text.replace("]", ")")
 
 
 tree = Tree.fromstring(text)
-tree.pretty_print(unicodelines=True, nodedist=10)   
-
+tree.pretty_print(unicodelines=True, nodedist=0)   
